a268442b89ed788153996d5766a1df76bbe1ca1a
Jayanttokas <55356411+Jayanttokas@users.noreply.github.com>
2020-06-10 12:12:31 +0530

    Updates

---
+++ precache-manifest.d7de5a311bb4c746179a5092263c92e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0c7041220c9bd0f06512e9bcea15269",
+    "url": "/DogTrainingApp/index.html"
+  },
+  {
+    "revision": "6322041e7b6b257ee20b",
+    "url": "/DogTrainingApp/static/js/2.11c22613.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/DogTrainingApp/static/js/2.11c22613.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5eeb7e1c80f58196d0c3",
+    "url": "/DogTrainingApp/static/js/main.bcf3f231.chunk.js"
+  },
+  {
+    "revision": "f5e46267531a38644ccf",
+    "url": "/DogTrainingApp/static/js/runtime-main.fd925d39.js"
+  }
+]);
